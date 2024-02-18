d1c569ddec6e3e65fa6a2f3a8ac05f5a51c2bbc7
吴琦 <wuqi@wuqideMac-mini.local>
2024-02-18 17:35:24 +0800

    fisrst commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Welcome to Manjia manjia.net</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+// 引入 vue.js 的默认值
 import { createApp } from 'vue'
+// 引入 App.vue 的默认值
 import App from './App.vue'
 
+// 创建一个新的 Vue 实例
 createApp(App).mount('#app')
